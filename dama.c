2f806fd50c5f1815a3a945487e7d3827f9c89598
aleganza <ganzarolli.alessio@gmail.com>
2022-10-09 19:17:48 +0200

    initBoard, starting menu

--- dama.c
+++ dama.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <conio.h>
 
 #define W 8
 #define H 8
 
+// clear all the matrix values
 void clearBoard(char mat[W][H]){
     for(int i=0; i<H; i++){
         for(int j=0; j<W; j++){
@@ -11,30 +13,65 @@ void clearBoard(char mat[W][H]){
     }
 }
 
+// print the board with the matrix values
 void printBoard(char mat[W][H]){
     for(int i=0; i<H; i++){
-        printf ("| ");
         for(int j=0; j<W; j++){
-            printf ("%c | ", mat[i][j]);
-        }
-        printf ("\n");
-        for(int j=0; j<W; j++){
-            printf ("____");
+            printf("[%c] ", mat[i][j]);
         }
         printf ("\n");
     }
 }
 
-/* void initBoard(char mat[][]){
-
-} */
+// initialize the matrix
+void initBoard(char mat[W][H]){
+    for(int i=0; i<H; i++){
+        for(int j=0; j<W; j++){
+            // check player 1 / player 2
+            if(i < H/2){
+                if( ((i+j) % 2 == 0) && ( (i != H/2) && (i != H/2 - 1) )){
+                    mat[i][j] = 'x';
+                }
+            }else{
+                if( ((i+j) % 2 == 0) && ( (i != H/2) && (i != H/2 - 1) )){
+                    mat[i][j] = 'o';
+                }
+            }
+        }    
+    }
+}
 
 int main(){
     char board[W][H];
+    int choice;
 
-    clearBoard(board);
-    printBoard(board);
-    /* initBoard(board); */
+    do{
+        printf("--- COMMAND LINE DAMA ---\n");
+        printf("\t-- menu --\n\n");
+
+        printf ("1 - New game\n");
+        printf ("2 - Exit\n\n");
+        printf ("Your choice: ");
+        scanf("%d",&choice);
+        if (choice == 2) break;
+
+        switch(choice){
+            case 1: {
+                clearBoard(board);
+                initBoard(board);
+                printBoard(board);
+                
+                break;
+            }
+            default: {
+                printf("Invalid choice");
+                break;
+            }
+        }
+        printf("\n\n");
+    }while(choice != 0);
+
+    printf ("Game ended");
 
     printf("\n");
     return 0;
